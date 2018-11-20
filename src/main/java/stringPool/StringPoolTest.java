b986e13f35911ab282fbc527c0dd34bbc4ee2e0e
rain7night <rainnight@outlook.com>
2018-11-20 17:55:30 +0800

    String pool

---
+++ src/main/java/stringPool/StringPoolTest.java
@@ -0,0 +1,32 @@
+package stringPool;
+
+public class StringPoolTest {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = "Hel" + "lo";
+        String s4 = "Hel" + new String("lo");
+        String s5 = new String("Hello");
+        String s6 = s5.intern();
+        String s7 = "H";
+        String s8 = "ello";
+        String s9 = s7 + s8;
+
+        // == ：比较两个对象是否为同一对象
+        System.out.println(s1 == s2);  // true
+        System.out.println(s1 == s3);  // true
+        System.out.println(s1 == s4);  // false
+        System.out.println(s1 == s9);  // false
+        System.out.println(s4 == s5);  // false
+        System.out.println(s1 == s6);  // true
+
+    }
+    /**
+     true
+     true
+     false
+     false
+     false
+     true
+     */
+}
